5af82dfc1cb272c6d874d298dec363c56e2f53f6
Pedro Andrés Chaparro Quintero <pedro.chaparro.2020@upb.edu.co>
2023-04-21 08:29:36 -0500

    chore: 💚 Check files with rome on new PRs

--- src/main.tsx
+++ src/main.tsx
@@ -1,6 +1,6 @@
+import './main.css';
 import React from 'react';
 import ReactDOM from 'react-dom/client';
-import './main.css';
 
 ReactDOM.createRoot(document.getElementById('root') as HTMLElement).render(
 	<React.StrictMode>
