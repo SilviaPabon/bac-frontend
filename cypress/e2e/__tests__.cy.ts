1dedaf1ffb4300dcf45d0bc1da704b4c7be4369e
Pedro Andrés Chaparro Quintero <pedro.chaparro.2020@upb.edu.co>
2023-04-24 21:03:42 -0500

    test(staff): ✅ Test watchmans editing residents

--- cypress/e2e/__tests__.cy.ts
+++ cypress/e2e/__tests__.cy.ts
@@ -142,4 +142,45 @@ describe('Staff tests', () => {
 			apartment,
 		);
 	});
+
+	// --- Update resident ---
+	it('Should be able to update a resident', () => {
+		// Generate data
+		const name = faker.name.fullName();
+		const mail = faker.internet.email();
+		const apartment = faker.datatype
+			.number({
+				min: 1,
+				max: 100,
+			})
+			.toString();
+
+		// Click the "Update" button in the last row
+		cy.get('table tbody tr:last-child td:last-child a').click();
+
+		// Check there are all the inputs
+		cy.get('input[name="name"]').should('exist');
+		cy.get('input[name="mail"]').should('exist');
+		cy.get('input[name="apartment"]').should('exist');
+
+		// Fill the inputs
+		cy.get('input[name="name"]').clear().type(name);
+		cy.get('input[name="mail"]').clear().type(mail);
+		cy.get('input[name="apartment"]').clear().type(apartment);
+
+		// Click on the update button
+		cy.get('input[type="submit"]').click();
+		cy.get('button[aria-label="close"]').click();
+
+		// Check the user is redirected to the /view-residents page
+		cy.url().should('include', '/view-residents');
+
+		// Check the resident is at the end of the table
+		cy.get('table tbody tr:last-child td:nth-child(2)').should('contain', name);
+		cy.get('table tbody tr:last-child td:nth-child(3)').should('contain', mail);
+		cy.get('table tbody tr:last-child td:nth-child(4)').should(
+			'contain',
+			apartment,
+		);
+	});
 });
